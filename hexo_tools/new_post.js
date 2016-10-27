a91f0fbf81e36e3e71f9a31960c7395f03f4e078
cwza <cwz0205a@gmail.com>
2016-10-27 15:15:54 +0800

    update new_post.js which add error handling for user not enter title

--- hexo_tools/new_post.js
+++ hexo_tools/new_post.js
@@ -5,17 +5,15 @@ const fs = require('fs');
 let args = process.argv.slice(2);
 
 let title = args[0];
+if(!title){throw 'please enter your title'};
 const hexoPath = '/Users/cwz/cwz/develop/hexo/hexo_blog/source/_posts/';
 let fileName = moment().format('YYYY-MM-DD') + '-' + title.split(' ').join('-') + '.org';
 let dateStr = moment().format('YYYY-MM-DD ddd HH:mm');
 
-let content = pug.renderFile('orgmode_template.pug', {
-    title: title,
-    dateStr: dateStr
-});
+let content = pug.renderFile('orgmode_template.pug', {title, dateStr});
 
 fs.writeFile(hexoPath + fileName, content, error => {
-    if(error){ 
+    if(error){
         console.log(error);
     } else{
         console.log('sucess');
